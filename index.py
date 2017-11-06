3c8bd5d42adc87c1d26e857aaed820696a87dc53
Luis de Bethencourt <luisbg@amazon.com>
2017-11-06 15:53:20 +0000

    Return random file

--- index.py
+++ index.py
@@ -9,15 +9,17 @@ def handler(event, context):
     print("Handle event %s" % event)
 
     objs = s3_client.list_objects_v2(Bucket="lbg-img")
-    print("amount of files: %s" % objs["KeyCount"])
+    count = objs["KeyCount"]
+    print("amount of files: %s" % count)
     print("")
 
-    for n in range(objs["KeyCount"]):
+    for n in range(count):
         print(objs["Contents"][n]["Key"])
 
     data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'output': 'Random image',
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'image': objs["Contents"][random.randint(0, count)]["Key"]
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
