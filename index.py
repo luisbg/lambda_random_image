e76520fad7c43fcf5f5e1b838e263c1c23e83176
Luis de Bethencourt <luisbg@amazon.com>
2017-11-06 16:09:37 +0000

    Return only image files

--- index.py
+++ index.py
@@ -10,16 +10,18 @@ def handler(event, context):
 
     objs = s3_client.list_objects_v2(Bucket="lbg-img")
     count = objs["KeyCount"]
-    print("amount of files: %s" % count)
-    print("")
 
+    images = []
     for n in range(count):
-        print(objs["Contents"][n]["Key"])
+        key = objs["Contents"][n]["Key"]
+        if key.endswith(".jpg"):
+            print(key)
+            images.append(key)
 
     data = {
         'output': 'Random image',
         'timestamp': datetime.datetime.utcnow().isoformat(),
-        'image': objs["Contents"][random.randint(0, count)]["Key"]
+        'image': images[random.randint(0, count - 1)]
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
