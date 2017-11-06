13cfa9b8aa3ea3cbd00706cd34194e832d12e066
Luis de Bethencourt <luisbg@amazon.com>
2017-11-06 14:40:59 +0000

    Display S3 bucket contents

--- index.py
+++ index.py
@@ -1,9 +1,19 @@
 import json
 import datetime
+import boto3
+import random
 
+s3_client = boto3.client('s3')
 
 def handler(event, context):
     print("Handle event %s" % event)
+
+    objs = s3_client.list_objects_v2(Bucket="lbg-img")
+    print("amount of files: %s" % objs["KeyCount"])
+    print("")
+
+    for n in range(objs["KeyCount"]):
+        print(objs["Contents"][n]["Key"])
 
     data = {
         'output': 'Hello World',
